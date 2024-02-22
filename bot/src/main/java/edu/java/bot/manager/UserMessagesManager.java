9c4417c23ef149a0331fdefcd7b194d1e45f56f3
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2024-02-23 01:25:09 +0500

    fix problems

--- bot/src/main/java/edu/java/bot/manager/UserMessagesManager.java
+++ bot/src/main/java/edu/java/bot/manager/UserMessagesManager.java
@@ -3,36 +3,23 @@ package edu.java.bot.manager;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.commands.Command;
-import edu.java.bot.commands.HelpCommand;
-import edu.java.bot.commands.ListCommand;
-import edu.java.bot.commands.StartCommand;
-import edu.java.bot.commands.TrackCommand;
-import edu.java.bot.commands.UntrackCommand;
 import edu.java.bot.resolver.TextResolver;
-import edu.java.bot.service.BotService;
-import jakarta.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
 public class UserMessagesManager implements MessagesManager {
     private final TextResolver textResolver;
-    private final BotService botService;
-    private List<Command> commands;
+    private final List<Command> commands;
 
-    @PostConstruct
-    public void initialize() {
-        commands = new ArrayList<>();
-        commands.add(new HelpCommand(textResolver, commands));
-        commands.add(new StartCommand(textResolver, botService));
-        commands.add(new TrackCommand(textResolver, botService));
-        commands.add(new UntrackCommand(textResolver, botService));
-        commands.add(new ListCommand(textResolver, botService));
+    @Autowired
+    public UserMessagesManager(TextResolver textResolver, List<Command> commands) {
+        this.textResolver = textResolver;
+        this.commands = commands;
     }
+
 
     @Override
     public List<Command> commands() {
--- bot/src/main/java/edu/java/bot/service/MemoryBotService.java
+++ bot/src/main/java/edu/java/bot/service/MemoryBotService.java
@@ -3,11 +3,12 @@ package edu.java.bot.service;
 import edu.java.bot.model.Link;
 import edu.java.bot.model.response.LinkResponse;
 import edu.java.bot.model.response.ListLinksResponse;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,23 +18,28 @@ public class MemoryBotService implements BotService {
 
     @Override
     public void registerUser(String name, Long id) {
-        usersLinks.put(id, new ArrayList<>());
+        usersLinks.put(id, new CopyOnWriteArrayList<>());
     }
 
     @Override
     public LinkResponse addLinkToUser(String url, Long userId) {
-        usersLinks.computeIfAbsent(userId, k -> new ArrayList<>()).add(new Link(url, UUID.randomUUID()));
+        usersLinks.computeIfAbsent(userId, k -> new CopyOnWriteArrayList<>())
+                  .add(new Link(url, UUID.randomUUID()));
         return new LinkResponse(true);
     }
 
     @Override
     public LinkResponse removeLinkFromUser(UUID linkId, Long userId) {
-        usersLinks.computeIfAbsent(userId, k -> new ArrayList<>()).removeIf(link -> link.uuid().equals(linkId));
+        usersLinks.computeIfAbsent(userId, k -> new CopyOnWriteArrayList<>())
+                  .removeIf(link -> link.uuid().equals(linkId));
         return new LinkResponse(true);
     }
 
     @Override
     public ListLinksResponse listLinks(Long userId) {
-        return new ListLinksResponse(usersLinks.get(userId));
+        if (usersLinks.isEmpty()) {
+            return new ListLinksResponse(List.of());
+        }
+        return new ListLinksResponse(Collections.unmodifiableList(usersLinks.get(userId)));
     }
 }
