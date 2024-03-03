aee7869559abbe0ade0abc9b49fe30d8be656117
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2024-03-03 19:26:13 +0500

    add REST api in scrapper-module

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -1,13 +1,24 @@
 package edu.java.configuration;
 
+import edu.java.client.bot.BotClient;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.core.publisher.Mono;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Scrapper API", description = "Scrapper API", version = "1.0.0"))
 public class ClientConfiguration {
     final ProviderConfig properties;
+
+    @Value("${bot.url:http://localhost:8090}")
+    private String botUrl;
 
     @Autowired
     public ClientConfiguration(ProviderConfig properties) {
@@ -16,11 +27,28 @@ public class ClientConfiguration {
 
     @Bean
     public WebClient githubWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder.baseUrl(properties.getGithub()).build();
+        return webClientBuilder.baseUrl(properties.getGithub())
+            .build();
     }
 
     @Bean
     public WebClient stackOverflowWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder.baseUrl(properties.getStackoverflow()).build();
+        return webClientBuilder.baseUrl(properties.getStackoverflow())
+            .build();
+    }
+
+    @Bean
+    public BotClient botClient() {
+        WebClient webClient = WebClient.builder()
+            .defaultStatusHandler(httpStatusCode -> true, clientResponse -> Mono.empty())
+            .defaultHeader("Content-Type", "application/json")
+            .baseUrl(botUrl)
+            .build();
+
+        var proxy =
+            HttpServiceProxyFactory
+                .builderFor(WebClientAdapter.create(webClient))
+                .build();
+        return proxy.createClient(BotClient.class);
     }
 }
