b11b792288f5ab14e87262ede496555ef9391b90
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2024-03-10 16:02:47 +0500

    remove listBean

--- bot/src/main/java/edu/java/bot/configuration/BeansConfiguration.java
+++ bot/src/main/java/edu/java/bot/configuration/BeansConfiguration.java
@@ -1,18 +1,8 @@
 package edu.java.bot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
-import edu.java.bot.commands.Command;
-import edu.java.bot.commands.HelpCommand;
-import edu.java.bot.commands.ListCommand;
-import edu.java.bot.commands.StartCommand;
-import edu.java.bot.commands.TrackCommand;
-import edu.java.bot.commands.UntrackCommand;
-import edu.java.bot.resolver.TextResolver;
-import edu.java.bot.service.BotService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,16 +25,5 @@ public class BeansConfiguration {
     @Bean
     public TelegramBot telegramBot(ApplicationConfig config) {
         return new TelegramBot(config.telegramToken());
-    }
-
-    @Bean
-    public List<Command> commandList(TextResolver textResolver, BotService botService) {
-        List<Command> commands = new ArrayList<>();
-        commands.add(new HelpCommand(textResolver, commands));
-        commands.add(new StartCommand(textResolver, botService));
-        commands.add(new TrackCommand(textResolver, botService));
-        commands.add(new UntrackCommand(textResolver, botService));
-        commands.add(new ListCommand(textResolver, botService));
-        return commands;
     }
 }
