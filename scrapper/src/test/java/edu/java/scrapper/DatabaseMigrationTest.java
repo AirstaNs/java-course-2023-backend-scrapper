d6737f2b6f759e3fc56f3fa47a3df38fc6774cdc
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2024-03-10 17:47:12 +0500

    done hw-4

---
+++ scrapper/src/test/java/edu/java/scrapper/DatabaseMigrationTest.java
@@ -0,0 +1,32 @@
+package edu.java.scrapper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DatabaseMigrationTest extends IntegrationTest {
+
+    @SneakyThrows
+    @Test
+    public void migrationLinkShouldCorrectWork() {
+        Connection connection = POSTGRES.createConnection("");
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM link");
+        ResultSet resultSet = statement.executeQuery();
+        Assertions.assertThat(resultSet.getMetaData().getColumnName(1)).isEqualTo("link_id");
+        Assertions.assertThat(resultSet.getMetaData().getColumnName(2)).isEqualTo("url");
+        Assertions.assertThat(resultSet.getMetaData().getColumnName(3)).isEqualTo("description");
+        Assertions.assertThat(resultSet.getMetaData().getColumnName(4)).isEqualTo("updated_at");
+        Assertions.assertThat(resultSet.getMetaData().getColumnName(5)).isEqualTo("last_checked_at");
+    }
+
+    @SneakyThrows
+    @Test
+    public void migrationChatShouldCorrectWork() {
+        Connection connection = POSTGRES.createConnection("");
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM chat");
+        Assertions.assertThat(statement.executeQuery().getMetaData().getColumnName(1)).isEqualTo("chat_id");
+    }
+}
